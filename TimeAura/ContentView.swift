fcbd7706b25c36964c8f52b065b0fe5e811d207d
nicolastx <normalteste13@gmail.com>
2025-12-05 14:49:19 -0300

    Initial Commit

---
+++ TimeAura/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeAura
+//
+//  Created by found on 05/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeAura/TimeAuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeAuraApp.swift
+//  TimeAura
+//
+//  Created by found on 05/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeAuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
